9be498dffb3a05402670a7f7a4a98d790b42d9a3
ozen-hub <info.ozenhub@gmail.com>
2024-01-23 12:54:01 +0530

    6

--- src/com/devstack/edu/controller/ProgramsFormController.java
+++ src/com/devstack/edu/controller/ProgramsFormController.java
@@ -1,4 +1,24 @@
 package com.devstack.edu.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ProgramsFormController {
+    public AnchorPane programFormContext;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
+    }
+    private void setUi(String location) throws IOException {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")));
+        Stage stage = (Stage) programFormContext.getScene().getWindow();
+        stage.setTitle("EduSmart");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+    }
 }
--- src/com/devstack/edu/controller/TrainersFormController.java
+++ src/com/devstack/edu/controller/TrainersFormController.java
@@ -1,4 +1,24 @@
 package com.devstack.edu.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TrainersFormController {
+    public AnchorPane trainersFormOnAction;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
+    }
+    private void setUi(String location) throws IOException {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")));
+        Stage stage = (Stage) trainersFormOnAction.getScene().getWindow();
+        stage.setTitle("EduSmart");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+    }
 }
