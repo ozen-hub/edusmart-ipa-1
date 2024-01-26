79beee5e5f32a99f5ed65a76393879da1c608118
ozen-hub <info.ozenhub@gmail.com>
2024-01-26 10:33:15 +0530

    10

--- src/com/devstack/edu/controller/LoginFormController.java
+++ src/com/devstack/edu/controller/LoginFormController.java
@@ -1,21 +1,55 @@
 package com.devstack.edu.controller;
 
+import com.devstack.edu.util.GlobalVar;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 
 public class LoginFormController {
     public AnchorPane loginFormContext;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
 
     public void signInOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("DashboardForm");
+        try{
+            //1 step
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //2 step
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/edusmart",
+                    "root","1234");
+            //3 step
+            String query = "SELECT email FROM user WHERE email=? AND password=?";
+            //4 step
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            //5 step
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2,txtPassword.getText());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                GlobalVar.userEmail= resultSet.getString(1);
+                setUi("DashboardForm");
+                return;
+            }
+            new Alert(Alert.AlertType.WARNING, "password or emails is wrong").show();
+
+        }catch (ClassNotFoundException | SQLException e){
+            new Alert(Alert.AlertType.ERROR, "Something went wrong").show();
+            e.printStackTrace();
+        }
+
+
     }
 
     public void createAccountOnAction(ActionEvent actionEvent) throws IOException {
