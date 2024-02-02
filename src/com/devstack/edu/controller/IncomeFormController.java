568cf5aa4e52a85cec2716746cd7d3aba4a48731
ozen-hub <info.ozenhub@gmail.com>
2024-02-02 11:19:24 +0530

    30

--- src/com/devstack/edu/controller/IncomeFormController.java
+++ src/com/devstack/edu/controller/IncomeFormController.java
@@ -3,6 +3,9 @@ package com.devstack.edu.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -10,6 +13,34 @@ import java.io.IOException;
 
 public class IncomeFormController {
     public AnchorPane incomeFormContext;
+    public AnchorPane chart;
+
+    public void initialize(){
+        loadChart();
+    }
+
+    private void loadChart() {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<Number,Number> lineChart=new LineChart<>(xAxis,yAxis);
+        XYChart.Series<Number,Number> series = new XYChart.Series<>();
+
+        series.getData().add(new XYChart.Data<>(1,500));
+        series.getData().add(new XYChart.Data<>(2,600));
+        series.getData().add(new XYChart.Data<>(3,300));
+        series.getData().add(new XYChart.Data<>(4,800));
+        series.getData().add(new XYChart.Data<>(5,200));
+        series.getData().add(new XYChart.Data<>(6,500));
+        series.getData().add(new XYChart.Data<>(7,600));
+        series.getData().add(new XYChart.Data<>(8,300));
+        series.getData().add(new XYChart.Data<>(9,800));
+        series.getData().add(new XYChart.Data<>(10,200));
+
+        lineChart.getData().add(series);
+
+        chart.getChildren().add(lineChart);
+    }
 
     public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashboardForm");
